26c32cbdea89e25fd9a8a2513c6c941a01395e30
Celie Baker <celie.n133@gmail.com>
2018-06-27 07:51:48 -0600

    formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,10 @@ import { WriteAReviewComponent } from './components/write-a-review/write-a-revie
 import { PhotoUploadComponent } from './components/photo-upload/photo-upload.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import {AngularFireAuthModule} from 'angularfire2/auth';
-import {environment} from "../environments/environment";
-import {AngularFireModule} from "angularfire2";
-import {AngularFirestoreModule} from "angularfire2/firestore";
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { environment } from "../environments/environment";
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
 
 
 
