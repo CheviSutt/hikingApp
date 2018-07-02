2b5b6aae8d79ff2b61f660234a6f6edabfba4c35
chevisutton <cheviksutton@gmail.com>
2018-07-02 10:37:53 -0600

    More FireBas'n

--- src/app/components/create-user/create-user.component.ts
+++ src/app/components/create-user/create-user.component.ts
@@ -2,42 +2,75 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import {HttpClient} from "@angular/common/http";
 import {CreateUserService} from "../../services/create-user.service";
+import { AngularFirestore, AngularFirestoreCollection} from "angularfire2/firestore";
+import {Observable} from "rxjs/index";
+import {map} from "rxjs/internal/operators";
 
+export interface Users {
+  address: string;
+  city: string;
+  firstName: string;
+  lastName: string;
+  state: string;
+  zipCode: string;
+  email: string;
+  password: string;
+}
+
+export interface UserId extends Users {
+  id: string;
+}
 @Component({
   selector: 'create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
+
 export class CreateUserComponent implements OnInit {
-
-  user: any;
-
+  user = this.setUser();//equal to returned value of setUser
   hide = true;
-
   email = new FormControl('', [Validators.required, Validators.email]);
 
+  private userCollection: AngularFirestoreCollection<Users>;
+  users: Observable<any[]>;
+
+  constructor(private http: HttpClient,
+              private createUserService: CreateUserService,
+              private db: AngularFirestore) {
+    this.userCollection = this.db.collection<Users>('userProfile');
+    this.users = this.userCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Users;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
+
+  ngOnInit() {
+  }
 
   getErrorMessage() {
     return this.email.hasError('required') ? 'You must enter a value' :
       this.email.hasError('email') ? 'Not a valid email' :
         '';
   }
-  constructor(private http: HttpClient,private createUserService: CreateUserService) { }
 
-  ngOnInit() {
-    this.getUserInfo();
-  }
-
-  getUserInfo(){
+  setUser() {
     this.user = {
-      address: "8 Mayfield Hill",
-      city: "Long Beach",
-      first_name: "Theresa",
-      last_name: "Fiddy",
-      state: "California",
-      zipcode: "90831",
+      address: "",
+      city: "",
+      firstName: "",
+      lastName: "",
+      state: "",
+      zipCode: "",
       email: "",
       password: ""
     }
+    return this.user;
   }
+    createUser(){
+      console.log(this.user);
+    }
+
 }
