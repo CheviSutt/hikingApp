d5ce13c09ecc58ad43f6aa5434b1dba9c4e9a43f
Celie Baker <celie.n133@gmail.com>
2018-07-13 08:18:43 -0600

    routing stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { auth } from 'firebase/app';
-import { Router } from "@angular/router";
+import { NavigationEnd, Router } from '@angular/router';
 import { Observable } from "rxjs/Rx";
 import { AngularFirestore, AngularFirestoreCollection } from "angularfire2/firestore";
 import { FormControl, Validators } from "@angular/forms";
 import { CreateUserService } from "./services/create-user.service";
-import { AuthService } from "./services/auth.service";
-
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/internal/operators";
 import * as firebase from "firebase";
@@ -36,6 +34,7 @@ export interface UserId extends Users {
 })
 export class AppComponent implements OnInit{
   createUser: Observable<any[]>;
+  public location = ''; //Celie's code. Don't delete.
 
   // 7-11-2018
   user = this.setUser();//equal to returned value of setUser
@@ -58,6 +57,16 @@ export class AppComponent implements OnInit{
     //-------
 
   ) {
+
+    //Celie's code. Don't delete:
+    this.location = router.url;
+    this.router.events.subscribe(route => {
+      if(route instanceof NavigationEnd) {
+          route.url = '/';
+      }
+    });
+    //------------------
+
     this.createUser = db.collection('userProfile').valueChanges();
     console.log(this.createUser);
     // 7-11-2018
@@ -106,11 +115,6 @@ export class AppComponent implements OnInit{
       // 7-11-2018
       console.log(this.user);
       this.userCollection.add(this.user);
-
-      firebase.auth().signInWithEmailAndPassword(this.email.toString(), this.password)
-        .catch(error => {
-          console.log(error);
-        });
 
       firebase.auth().signOut().then(function() {
         // Sign-out successful.
